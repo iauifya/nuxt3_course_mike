e966336baebcdcfbf81a0854f64bab9d201f19b8
P200883\iauifya <iauifya@staff.com.tw>
2024-09-10 16:49:33 +0800

    CH09專案上傳

--- CH09/nuxt3-https/nuxt.config.ts
+++ CH09/nuxt3-https/nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
+const config = {
+    devServer: {
+        https: {
+            key: './https/localhost+3-key.pem',
+            cert: './https/localhost+3.pem'
+        }
+    },
+    "vite": {
+        "server": {
+            "proxy": {},
+        },
+    },
+}
+console.log(process.env.NODE_ENV)
+if(process.env.NODE_ENV === 'development') {
+    config.vite.server.proxy = {
+        '/VsWeb/api': {
+            "target": 'https://www.vscinemas.com.tw/',
+            "changeOrigin": true,
+        },
+    }
+    console.log(config.vite.server.proxy)
+}
 
-export default defineNuxtConfig({})
+export default defineNuxtConfig(config)
 
